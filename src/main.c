dbfcd5653ac805b2dd42a198533a2ca5c8c438a7
Zertmark <zert.hello@gmail.com>
2024-08-17 19:48:10 +0300

    Added token file

--- src/main.c
+++ src/main.c
@@ -1,9 +1,22 @@
 #include "bot.h"
-#define TOKEN "5766970401:AAGTrrHEL3BIO7sj7T2oYGMxXB-FsdF6zmA"
-
+char* read_token()
+{
+    FILE *file_ptr = fopen("token","r");
+    fseek(file_ptr, 0, SEEK_END);
+    size_t f_size = ftell(file_ptr);
+    rewind(file_ptr);
+    char* token = calloc(f_size, sizeof(char)); //this is dangerous way to read file
+    for(size_t i = 0; i < f_size; i++)
+    {
+        token[i] = fgetc(file_ptr);
+    }
+    token[f_size] = 0;
+    fclose(file_ptr);
+    return token;
+}
 int main(int argc, char *argv[])
 {
-    if (!tryTocreateBot(TOKEN))
+    if (!tryTocreateBot(read_token()))
     {
     	printf("Failed to login with token\nExiting...\n");
     	return -1;
